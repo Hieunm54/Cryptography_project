610162e08d9e383cd2bc3bacaafa64c656406bf0
TranDucAnh2000 <tfcdeszaq2000@gmail.com>
2021-07-14 22:13:20 +0700

    .

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -3,31 +3,22 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from 'react-bootstrap/Image';
+
+import image from './bitcoin.jpg';
 
 const Header = () => {
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Image src={image} style={{width: '30px'}} />
+          <Navbar.Brand href="#home">HaHaCoin</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#settings">Settings</Nav.Link>
+              <Nav.Link href="#createTransaction">Create Trantraction</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
